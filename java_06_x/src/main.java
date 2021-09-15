f515efba010c46edace43f4bc941fa82807cac4b
Barrazal <maxi1010@gmx.at>
2021-09-15 09:45:27 +0200

    update Uebung 6 aufgrund IntelliJ upgrade

--- java_06_x/src/main.java
+++ java_06_x/src/main.java
@@ -5,64 +5,22 @@ public class main {
     public static void main(String[] args) {
 
 
-        //Aufgabe 6.1 - Berrechnung des Maximums
-        //
-        //Schreibe ein kleines Programm, welches eine Liste von Zahlen über die Konsole zahlenweise einliest.
-        // Mit 'q' solltest du die Eingabe beenden können. Errechne dir danach aus der eben eingelesenen Liste das Maximum und gib es auf der Konsole aus.
+//Aufgabe 6.1 - Berrechnung des Maximums
+//
+//Schreibe ein kleines Programm, welches eine Liste von Zahlen über die Konsole zahlenweise einliest.
+// Mit 'q' solltest du die Eingabe beenden können. Errechne dir danach aus der eben eingelesenen Liste das Maximum und gib es auf der Konsole aus.
 
 
         //https://www.w3schools.com/java/java_arraylist.asp
         Scanner scan = new Scanner(System.in);
+//Erstellt eine ArrayList, welche sämtliche Consoleneingaben des Scanner übernimmt
 
-        ArrayList<String> array1 = new ArrayList<String>();
+        ArrayList arr1 = new ArrayList();
+        arr1.add(scan.nextLine());
 
+//Gibt die ArrayList aus
+        System.out.println(arr1);
 
-
-
-
-  /*
-        for (int i = 0; i < array1.size(); i++) {
-            System.out.println(array1.get(i));
-        }
-
-   */
-        /*
-
-        //----------------------------------------------------------
-        ArrayList arrayL1 = new ArrayList();
-        String[] array1 = {};
-
-
-        for (int i = 0;  i > -1 ; i++) {
-            System.out.println("Gib bitte einen Wert ein, oder beende das Programm mit einem q!");
-            array1[i] = scan.nextLine();
-
-            if ( array1[i] == "q" || array1[i] == "Q") {
-                i = -2;
-            }
-        }
-        System.out.println(arrayL1);
-
-        do {
-            System.out.println("Gib eine nummer ein, oder beende die eingabe mit einem q!");
-            number1 = scan.nextByte();
-        }while (number1 != letterQ);
-        
-
-//----------------------------------------
-
-        for (int i = 0; i == -1; i++){
-            int number1 = scan.nextInt();
-            String letterIn = scan.nextLine();
-            if(letterIn == "q"){
-                i = -1;
-            }else{
-                array1[i] = number1;
-            }
-            System.out.println(i);
-        }
-        System.out.println(array1);
-*/
 
     }
 }
