afcab633912561b1031b4713cdb27428ac624054
Barrazal <maxi1010@gmx.at>
2021-10-05 16:09:26 +0200

    Mengenlehre mit HashSet und Wörterbuch mit HashMap

---
+++ Mengenlehre/src/Mengenlehre.java
@@ -0,0 +1,55 @@
+//import java.util.Collections;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Mengenlehre {
+    public static void main(String[] args) {
+        Integer[] a = {49, 30, 14, 47, 13, 2, 12, 29, 19, 11, 15, 39, 43, 45, 34};
+        Integer[] b = {39, 33, 38, 14, 4, 32, 40, 25, 17, 46, 35, 6, 2, 12, 49};
+        Integer[] c = {41, 12, 5, 35, 42, 28, 47, 20, 26, 24, 50, 40, 14, 17, 10};
+
+        System.out.println();
+        System.out.println("Vereinigungsmenge von 'A u B u C' ist: " + unificationQuantity(a, b, c));
+        System.out.println();
+        System.out.println("Schnittmengen  von 'A u B u C' ist: " + intersections(a, b, c));
+        System.out.println();
+        System.out.println("Differenzmenge von (A / B) / C : " + differences(a ,b ,c));
+        System.out.println();
+        System.out.println("Differenzmenge von (B / C) / A : " + differences(b ,c ,a));
+        System.out.println();
+        System.out.println("Differenzmenge von (C / B) / A : " + differences(c ,a ,b));
+    }
+
+    private static HashSet<Integer> unificationQuantity(Integer[] a, Integer[] b, Integer[] c) {
+        HashSet<Integer> mergNum = new HashSet<Integer>();
+        mergNum.addAll(Arrays.asList(a));
+        mergNum.addAll(Arrays.asList(b));
+        mergNum.addAll(Arrays.asList(c));
+        return mergNum;
+    }
+    //private static Set<Integer> unificationQuantity(Integer[] a, Integer[] b, Integer[] c) {
+    //    Set<Integer> mergNum = new HashSet<Integer>();
+    //    Collections.addAll(mergNum, a);
+    //    Collections.addAll(mergNum, b);
+    //    Collections.addAll(mergNum, c);
+    //    return mergNum;
+    //}
+
+    private static HashSet<Integer> intersections(Integer[] a, Integer[] b, Integer[] c) {
+        HashSet<Integer> resNumb = new HashSet<Integer>();
+        resNumb.addAll(Arrays.asList(a));
+        resNumb.retainAll(Arrays.asList(b));
+        resNumb.retainAll(Arrays.asList(c));
+        return resNumb;
+    }
+
+
+    private static HashSet<Integer> differences(Integer[] one, Integer[] two, Integer[] three) {
+        HashSet<Integer> resNumb = new HashSet<Integer>();
+        resNumb.addAll(Arrays.asList(one));
+        resNumb.removeAll(Arrays.asList(two));
+        resNumb.removeAll(Arrays.asList(three));
+        return resNumb;
+    }
+}
---
+++ Wörterbuch DE - EN/src/Dictionary.java
@@ -0,0 +1,102 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Dictionary {
+    public static void main(String[] args) {
+        HashMap<String, String> h1 = new HashMap<String, String>();
+        onStart(h1);
+
+    }
+
+    public static void onStart(HashMap<String,String> h1){
+        Scanner scan1 = new Scanner(System.in);
+        int addDelNumb = 0;
+        System.out.println("Wähle aus folgenden Optionen:");
+        System.out.println("Zum Anzeigen des Aktuellen Inhaltes bestätige mit 0!");
+        System.out.println("Füge ein Wort hinzu bestätige mit 1!");
+        System.out.println("Soll ein Wort gelöscht werden, bestätige mit 2!");
+        System.out.println("Beende das Programm mit dem bestätigen von 3!");
+        try {
+            addDelNumb = scan1.nextInt();
+            loop(addDelNumb,h1);
+        } catch (Exception ex) {
+            System.out.println("Du hast keine Korrekte Zahl eingegeben!");
+            System.out.println();
+            onStart(h1);
+        }
+    }
+
+
+
+    private static void loop(int addDelNumb,HashMap<String,String> h1) {
+        Scanner scan1 = new Scanner(System.in);
+
+        if (addDelNumb == 0) {
+            System.out.println(h1);
+            onStart(h1);
+        } else if (addDelNumb == 1) {
+            System.out.println("Trage das Wort ein, welches hinzugefügt werden soll in folgendem Format ein: DEUTSCH,ENGLISCH");
+            String[] translate = scan1.next().split(",");
+            addTranslation(translate, h1);
+            onStart(h1);
+        } else if (addDelNumb == 2) {
+            System.out.println("Trage das Wort ein, welches gelöscht werden soll in folgendem Format ein: DEUTSCH,ENGLISCH");
+            String[] translate = scan1.next().split(",");
+            removeTranslation(translate, h1);
+            onStart(h1);
+        } else if (addDelNumb == 3) {
+            System.out.println("Das Programm wurde beendet!");
+        } else {
+            System.out.println("Es wurde nicht erkannt, ob ein Eintrag hinzugefügt, ausgelesen oder gelöscht werden soll!");
+            onStart(h1);
+        }
+
+
+    }
+
+
+    private static HashMap<String, String> addTranslation(String[] translate, HashMap h1) {
+        h1.put(translate[0], translate[1]);
+        return h1;
+    }
+
+    private static HashMap<String, String> removeTranslation(String[] translate, HashMap h1) {
+        h1.remove(translate[0], translate[1]);
+        return h1;
+    }
+
+
+}
+
+/*
+Input Frage: soll ein Wort hinzugefügt oder gelöscht werden.
+Input Wort: Trage das Wort in folgendem Format ein: Deutsch Englisch
+
+
+
+
+
+überprüfe ob hinzugefügt oder gelöscht ausgewählt wurde.
+wenn hinzugefügt ausgewählt wurde, erstelle in einer Vordefinierten Hashmap einen neuen eintragen mit folgendem Syntax:
+erstes Wort/ Eingabe = Key
+zweites Wort/Eingabe = Values
+
+Überprüfe ob das Wort korrekt eingegeben wurde.
+    falls nicht und es handelt sich um ein Wort, welches hinzugefügt werden soll
+        dann gibt eine meldung aus, dass das eingebenen Falsch ist
+    falls nicht und es handelt sich um ein Wort, welches gelöscht werden soll
+        dann überprüfe, ob das Wort ein Key oder ein Value ist.
+    falls ja, dann führe die Operation entsprechend der Methoden aus
+
+
+hinzufügen:
+überprüfe ob der Key bereits vorhanden ist.
+    falls ja, dann überprüfe ob das Value gleich dem eingegebenen ist.
+        falls ja, gib rückmeldung: diese Übersetzung ist bereits eingetragen.
+        falls nein, füge diese Übersetzung als neue Values für einen neuen Key hinzu.
+    falls nein, dann erstelle einen Neuen Key mit dem entsprechenden Value.
+
+löschen:
+übernimm den Wert der überprüfung ob sie korrekt eingegeben ist und entferne den entsprechenden wert aus der Hashmap
+
+ */
