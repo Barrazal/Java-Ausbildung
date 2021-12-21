d589099f3dbf1cb5b9fe6d895e7a0b7c2477ce1a
Barrazal <maxi1010@gmx.at>
2021-12-21 14:10:44 +0100

    CSV I/O

---
+++ SchoolDAO/src/SchoolCSV.java
@@ -0,0 +1,15 @@
+/*
+https://www.tutorialspoint.com/java/java_files_io.htm
+https://mail.codejava.net/coding/java-code-example-to-export-from-database-to-csv-file
+ */
+
+
+import java.io.*;
+
+public class SchoolCSV {
+FileWriter file1 = new FileWriter("testfile.txt");
+
+
+    public SchoolCSV() throws IOException {
+    }
+}
